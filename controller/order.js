6d4f2df2d48b890a4cbf1a55f83f9fea1eea1aa3
chamika-damith <chamikadamith9@gmail.com>
2024-05-09 13:50:17 +0530

    order purchase

--- controller/order.js
+++ controller/order.js
@@ -1,1 +1,28 @@
-$()
+import orderDetailsModel from "../model/orderDetailsModel.js";
+import { orderDetails} from "../db/db.js";
+
+
+$("#btnPurchase").on('click', () => {
+
+    let alertConfrim = confirm('Do you really want to Purchase this item');
+    if (alertConfrim==true) {
+
+        var orderId = $('#orderId').val();
+        var orderDate = $('#orderDate').val();
+        var cusIdOption = $('#cusIdOption').val();
+        var itemIdOption = $('#itemIdOption').val();
+        var orderQty = $('#orderQty').val();
+        var total = $('#total').val();
+        var txtCash = $('#txtCash').val();
+        var txtDiscount = $('#txtDiscount').val();
+
+        let orderDetailObj=new orderDetailsModel(
+            orderId,orderDate,cusIdOption,itemIdOption,orderQty,total,txtCash,txtDiscount
+        );
+
+        orderDetails.push(orderDetailObj);
+
+
+
+    }
+});
